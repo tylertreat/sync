5ef82c350bd9581e092b64f43fd46e65ea749afc
Tyler Treat <ttreat31@gmail.com>
2014-08-30 13:31:04 -0500

    Put mutex and sem on struct instead of pointers

--- counting_sem.h
+++ counting_sem.h
@@ -7,9 +7,9 @@
 // Defines a counting semaphore.
 typedef struct counting_sem_t
 {
-    int              count;
-    pthread_mutex_t* mu;
-    pthread_cond_t*  cond;
+    int             count;
+    pthread_mutex_t mu;
+    pthread_cond_t  cond;
 } counting_sem_t;
 
 // Initializes a semaphore and sets its initial value. Sets errno and returns
